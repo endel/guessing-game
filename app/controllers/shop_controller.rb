26a40d528616b77fba2ab052adab51ef4a949557
Endel Dreyer <endel.dreyer@gmail.com>
2012-10-14 20:40:15 -0300

    Add vote badge

--- app/controllers/shop_controller.rb
+++ app/controllers/shop_controller.rb
@@ -1,21 +1,21 @@
 class ShopController < ApplicationController
-  
+
   def index
     @specials = Special.all
   end
-  
+
   # POST
   def buy
     cart = params[:available_specials].select {|key, value| params[:available_specials][key].present? }
     total = 0
     cart.each {|v,k| total += (Special.find(v).price.to_i * k.to_i) }
     puts "CART=>#{cart.inspect} - TOTAL=>#{total}"
-    
+
     # Verify if the user has money to buy all items
     if @user.coins >= total
       # Buy the selected helpers
       cart.each do |s|
-        
+
         special = Special.find(s.first)
         puts "Buying=>#{special.name}"
         @user.buy(special, s[1] ||= 0)
@@ -26,5 +26,5 @@ class ShopController < ApplicationController
     end
 
   end
-  
+
 end
